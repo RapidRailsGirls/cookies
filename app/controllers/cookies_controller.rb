0eb14d86baf8ffc20ababb77b9712aba56f8e549
Erik Michaels-Ober <sferik@gmail.com>
2014-08-13 15:34:41 +0200

    Added create action to CookiesController

--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -1,2 +1,12 @@
 class CookiesController < ApplicationController
+
+  def create
+    num_cookies = params['num_cookies'].to_i
+    num_cookies.times do
+      Cookie.create
+    end
+    flash[:info] = "#{num_cookies} #{num_cookies == 1 ? 'cookie was' : 'cookies were'} created"
+    redirect_to root_path
+  end
+
 end
