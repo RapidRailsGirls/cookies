59568e1c453b648ac1555a0cf7d2d0f7a0398565
Erik Michaels-Ober <sferik@gmail.com>
2014-08-13 17:34:33 +0200

    Handle success and errors from Ajax response

--- app/assets/javascripts/cookies.js
+++ app/assets/javascripts/cookies.js
@@ -5,6 +5,14 @@ $(function() {
         type: "POST",
         data: {
           "num_cookies": $('#num_cookies').val()
+        },
+        success: function(data) {
+          $('#alert').css('color', 'green');
+          $('#alert').html(data['message']);
+        },
+        error: function(jqXHR) {
+          $('#alert').css('color', 'red');
+          $('#alert').html(jqXHR.responseJSON['message']);
         }
       }
     );
--- app/controllers/cookies_controller.rb
+++ app/controllers/cookies_controller.rb
@@ -2,17 +2,30 @@ class CookiesController < ApplicationController
 
   def create
     num_cookies = params['num_cookies'].to_i
-    num_cookies.times do
-      Cookie.create
-    end
-    message = "#{num_cookies} #{num_cookies == 1 ? 'cookie was' : 'cookies were'} created"
-    respond_to do |format|
-      format.html do
-        flash[:info] = message
-        redirect_to root_path
+    if rand(2) == 0
+      num_cookies.times do
+        Cookie.create
       end
-      format.json do
-        render :json => {"message" => message}
+      message = "#{num_cookies} #{num_cookies == 1 ? 'cookie was' : 'cookies were'} created"
+      respond_to do |format|
+        format.html do
+          flash[:info] = message
+          redirect_to root_path
+        end
+        format.json do
+          render :json => {"message" => message}
+        end
+      end
+    else
+      message = "An error happened!"
+      respond_to do |format|
+        format.html do
+          flash[:info] = message
+          redirect_to root_path
+        end
+        format.json do
+          render :json => {"message" => message}, :status => 500
+        end
       end
     end
   end
